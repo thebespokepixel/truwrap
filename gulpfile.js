fd52d865fb06e2fca62f86e5493d1c7c5be648e0
Mark Griffiths <mark@thebespokepixel.com>
2016-04-22 18:09:21 +0100

    Fix gulpfile
    Signed-off-by: Mark Griffiths <mark@thebespokepixel.com>

--- gulpfile.js
+++ gulpfile.js
@@ -10,26 +10,31 @@ const cordial = require('@thebespokepixel/cordial')
 gulp.task('default', ['coffee'])
 
 // Versioning
-gulp.task('bump', cordial.version.build.inc)
-gulp.task('reset', cordial.version.build.reset)
-gulp.task('write', cordial.version.build.write)
+gulp.task('inc-build', cordial.version.build.inc)
+gulp.task('reset-build', cordial.version.build.reset)
+gulp.task('bump', ['inc-build'], cordial.version.build.write)
+gulp.task('reset', ['reset-build'], cordial.version.build.write)
+gulp.task('test-reset', ['test'], cordial.version.build.resetWrite)
 
 // Comtranspilationatting
-gulp.task('coffee', ['bump', 'write'], cordial.compile.coffee(['src/**/*.coffee'], './'))
+gulp.task('coffee', ['bump'], cordial.compile.coffee(['src/**/*.coffee'], './'))
 
 // Tests
-gulp.task('test', cordial.test.ava(['test/*']))
+gulp.task('test', ['xo'], cordial.test.ava(['test/*.js']))
+gulp.task('xo', cordial.test.xo(['**/*.js', '!{src,lib,scripts,node_modules}/**/*', '!index.js']))
+
+// npm
+gulp.task('publish', cordial.npm.publish)
 
 // Git
 gulp.task('commit', cordial.git.commitAll)
 gulp.task('push', cordial.git.pushAll('origin'))
 gulp.task('backup', ['push'], cordial.git.pushAll('backup'))
-
-// npm
-gulp.task('publish', ['test'], cordial.npm.publish)
+gulp.task('publish-push', ['publish'], cordial.git.pushAll('origin'))
+gulp.task('publish-backup', ['publish-push'], cordial.git.pushAll('backup'))
 
 // Guppy Hooks
-gulp.task('post-flow-release-start', ['reset', 'write'], cordial.flow.release.start)
-gulp.task('post-flow-release-finish', ['publish', 'push'])
+gulp.task('post-flow-release-start', ['reset'], cordial.flow.release.start)
+gulp.task('post-flow-release-finish', ['publish-backup'])
 gulp.task('filter-flow-release-start-version', cordial.flow.release.versionFilter)
 gulp.task('filter-flow-release-finish-tag-message', cordial.flow.release.tagFilter)
