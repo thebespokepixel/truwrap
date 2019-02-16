1091ca2df41b48e5c9b11943deb4f7a2a8e9ac53
Mark Griffiths <mark@thebespokepixel.com>
2019-02-16 19:48:11 +0000

    Update tests

--- test/cli-60.js
+++ test/cli-60.js
@@ -1,15 +1,14 @@
 import path from 'path'
-import shell from 'shelljs'
+import {readFileSync} from 'fs'
+import {exec} from 'child_process'
 import test from 'ava'
 import pkg from '../package'
 
 const expectedVersion = pkg.version
 
 test.cb(`Module name/version is '${pkg.name} v${expectedVersion}'.`, t => {
-	shell.exec('./bin/truwrap -vv', {
-		silent: true
-	}, (code_, out_) => {
-		t.is(code_, 0)
+	exec('./bin/truwrap -vv', (error_, out_) => {
+		t.is(error_, null)
 		t.is(out_.trim(), `${pkg.name} v${expectedVersion}`)
 		t.end()
 	})
@@ -19,83 +18,90 @@ const width = path.basename(__filename, '.js').split('-')[1] // 10, 20, 40, 60, 
 
 if ([40, 60, 80, 100].indexOf(Number(width)) !== -1) {
 	test.cb(`Panel: width = ${width} left = 0, right = 0`, t => {
-		const fixture = shell.cat(`./test/fixtures/out/panel-${width}-0-0.txt`).toString()
-		shell.exec(`cat ./test/fixtures/in/panel.txt | ./bin/truwrap --panel --width ${width} --left 0 --right 0`, {
-			silent: true
-		}, (code_, out_) => {
-			t.is(code_, 0)
-			t.is(out_, fixture)
-			t.end()
-		})
+		const fixture = readFileSync(`./test/fixtures/out/panel-${width}-0-0.txt`, 'utf8')
+		exec(
+			`cat ./test/fixtures/in/panel.txt | ./bin/truwrap --panel --width ${width} --left 0 --right 0`,
+			(error_, out_) => {
+				t.is(error_, null)
+				t.is(out_, fixture)
+				t.end()
+			}
+		)
 	})
 }
 
 [0, 1, 5].forEach(right => {
 	[0, 1, 2, 5, 10].forEach(left => {
 		test.serial.cb(`Soft wrap to width = ${width}, left = ${left}, right = ${right}`, t => {
-			const fixture = shell.cat(`./test/fixtures/out/lorem-soft-${width}-${left}-${right}.txt`).toString()
-			shell.exec(`cat ./test/fixtures/in/lorem.txt | ./bin/truwrap --left ${left} --right ${right} --width ${width}`, {
-				silent: true
-			}, (code_, out_) => {
-				t.is(code_, 0)
-				t.is(out_, fixture)
-				t.end()
-			})
+			const fixture = readFileSync(`./test/fixtures/out/lorem-soft-${width}-${left}-${right}.txt`, 'utf8')
+			exec(
+				`cat ./test/fixtures/in/lorem.txt | ./bin/truwrap --left ${left} --right ${right} --width ${width}`,
+				(error_, out_) => {
+					t.is(error_, null)
+					t.is(out_, fixture)
+					t.end()
+				}
+			)
 		})
 
 		test.serial.cb(`Soft wrap tabbed source to width = ${width}, left = ${left}, right = ${right}`, t => {
-			const fixture = shell.cat(`./test/fixtures/out/tabbed-soft-${width}-${left}-${right}.txt`).toString()
-			shell.exec(`cat ./test/fixtures/in/tabbed.txt | ./bin/truwrap --left ${left} --right ${right} --width ${width}`, {
-				silent: true
-			}, (code_, out_) => {
-				t.is(code_, 0)
-				t.is(out_, fixture)
-				t.end()
-			})
+			const fixture = readFileSync(`./test/fixtures/out/tabbed-soft-${width}-${left}-${right}.txt`, 'utf8')
+			exec(
+				`cat ./test/fixtures/in/tabbed.txt | ./bin/truwrap --left ${left} --right ${right} --width ${width}`,
+				(error_, out_) => {
+					t.is(error_, null)
+					t.is(out_, fixture)
+					t.end()
+				}
+			)
 		})
 
 		test.serial.cb(`Hard wrap to width = ${width}, left = ${left}, right = ${right}`, t => {
-			const fixture = shell.cat(`./test/fixtures/out/lorem-hard-${width}-${left}-${right}.txt`).toString()
-			shell.exec(`cat ./test/fixtures/in/lorem.txt | ./bin/truwrap --left ${left} --right ${right} --width ${width} --mode hard`, {
-				silent: true
-			}, (code_, out_) => {
-				t.is(code_, 0)
-				t.is(out_, fixture)
-				t.end()
-			})
+			const fixture = readFileSync(`./test/fixtures/out/lorem-hard-${width}-${left}-${right}.txt`, 'utf8')
+			exec(
+				`cat ./test/fixtures/in/lorem.txt | ./bin/truwrap --left ${left} --right ${right} --width ${width} --mode hard`,
+				(error_, out_) => {
+					t.is(error_, null)
+					t.is(out_, fixture)
+					t.end()
+				}
+			)
 		})
 
 		test.serial.cb(`Hard wrap tabbed source to width = ${width}, left = ${left}, right = ${right}`, t => {
-			const fixture = shell.cat(`./test/fixtures/out/tabbed-hard-${width}-${left}-${right}.txt`).toString()
-			shell.exec(`cat ./test/fixtures/in/tabbed.txt | ./bin/truwrap --left ${left} --right ${right} --width ${width} --mode hard`, {
-				silent: true
-			}, (code_, out_) => {
-				t.is(code_, 0)
-				t.is(out_, fixture)
-				t.end()
-			})
+			const fixture = readFileSync(`./test/fixtures/out/tabbed-hard-${width}-${left}-${right}.txt`, 'utf8')
+			exec(
+				`cat ./test/fixtures/in/tabbed.txt | ./bin/truwrap --left ${left} --right ${right} --width ${width} --mode hard`,
+				(error_, out_) => {
+					t.is(error_, null)
+					t.is(out_, fixture)
+					t.end()
+				}
+			)
 		})
 
 		test.serial.cb(`Keep wrap to width = ${width}, left = ${left}, right = ${right}`, t => {
-			const fixture = shell.cat(`./test/fixtures/out/ls-keep-${width}-${left}-${right}.txt`).toString()
-			shell.exec(`cat ./test/fixtures/in/ls.txt | ./bin/truwrap --left ${left} --right ${right} --width ${width} --mode keep`, {
-				silent: true
-			}, (code_, out_) => {
-				t.is(code_, 0)
-				t.is(out_, fixture)
-				t.end()
-			})
+			const fixture = readFileSync(`./test/fixtures/out/ls-keep-${width}-${left}-${right}.txt`, 'utf8')
+			exec(
+				`cat ./test/fixtures/in/ls.txt | ./bin/truwrap --left ${left} --right ${right} --width ${width} --mode keep`,
+				(error_, out_) => {
+					t.is(error_, null)
+					t.is(out_, fixture)
+					t.end()
+				}
+			)
 		})
 
 		test.serial.cb(`Stamp: "Hello %s!" World width = ${width}, left = ${left}, right = ${right}`, t => {
-			const fixture = shell.cat(`./test/fixtures/out/stamp-${width}-${left}-${right}.txt`).toString()
-			shell.exec(`cat ./test/fixtures/in/lorem.txt | ./bin/truwrap -s "Hello %s!" World --left ${left} --right ${right} --width ${width}`, {
-				silent: true
-			}, (code_, out_) => {
-				t.is(code_, 0)
-				t.is(out_, fixture)
-				t.end()
-			})
+			const fixture = readFileSync(`./test/fixtures/out/stamp-${width}-${left}-${right}.txt`, 'utf8')
+			exec(
+				`cat ./test/fixtures/in/lorem.txt | ./bin/truwrap -s "Hello %s!" World --left ${left} --right ${right} --width ${width}`,
+				(error_, out_) => {
+					t.is(error_, null)
+					t.is(out_, fixture)
+					t.end()
+				}
+			)
 		})
 	})
 })
