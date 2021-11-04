75eaef89238dfa75e916fd54fdb8adf11cc616c2
Mark Griffiths <mark@thebespokepixel.com>
2021-11-04 21:53:09 +0000

    Add image test

---
+++ test/image.js
@@ -0,0 +1,24 @@
+import {join, dirname} from 'node:path'
+import {fileURLToPath} from 'node:url'
+import {PassThrough} from 'node:stream'
+import test from 'ava'
+import {truwrap, createImage} from '../index.js'
+
+const image = createImage({
+	name: 'test',
+	file: join(dirname(fileURLToPath(import.meta.url)), '/media/test.png'),
+	height: 1,
+})
+
+test('Testing image handling', t => {
+	const tw = truwrap({
+		mode: 'container',
+	})
+
+	tw.write(image.render({
+		nobreak: false,
+		align: 1,
+	}))
+
+	t.snapshot(tw.end())
+})
